0e409f0904c9f785b5dd6be8736d7f40d136d019
Dirk Fortmeier <mail@dfortmeier.de>
2015-03-28 11:21:29 +0100

    replaced cube by quad

--- weltfrieden.c
+++ weltfrieden.c
@@ -10,33 +10,7 @@
 
 extern int server_init(void);
 
-GLfloat light_diffuse[] = {1.0, 0.0, 0.0, 1.0};  /* Red diffuse light. */
-GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};  /* Infinite light location. */
-GLfloat n[6][3] = {  /* Normals for the 6 faces of a cube. */
-  {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
-  {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
-GLint faces[6][4] = {  /* Vertex indices for the 6 faces of a cube. */
-  {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
-  {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
-GLfloat v[8][3];  /* Will be filled in with X,Y,Z vertexes. */
-
-
 float cube_angle = 60;
-void
-drawBox(void)
-{
-  int i;
-
-  for (i = 0; i < 6; i++) {
-    glBegin(GL_QUADS);
-    glNormal3fv(&n[i][0]);
-    glVertex3fv(&v[faces[i][0]][0]);
-    glVertex3fv(&v[faces[i][1]][0]);
-    glVertex3fv(&v[faces[i][2]][0]);
-    glVertex3fv(&v[faces[i][3]][0]);
-    glEnd();
-  }
-}
 
 void
 display(void)
@@ -49,42 +23,27 @@ display(void)
   g = cube_angle;
   glClearColor(r,g,b,1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   /* Setup cube vertex data. */
-  v[0][0] = v[1][0] = v[2][0] = v[3][0] = -1;
-  v[4][0] = v[5][0] = v[6][0] = v[7][0] = 1;
-  v[0][1] = v[1][1] = v[4][1] = v[5][1] = -1;
-  v[2][1] = v[3][1] = v[6][1] = v[7][1] = 1;
-  v[0][2] = v[3][2] = v[4][2] = v[7][2] = 1;
-  v[1][2] = v[2][2] = v[5][2] = v[6][2] = -1;
-
-  /* Enable a single OpenGL light. */
-  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-  glEnable(GL_LIGHT0);
-  glEnable(GL_LIGHTING);
 
   /* Use depth buffering for hidden surface elimination. */
   glEnable(GL_DEPTH_TEST);
 
   /* Setup the view of the cube. */
   glMatrixMode(GL_PROJECTION);
-  gluPerspective( /* field of view in degree */ 40.0,
-    /* aspect ratio */ 1.0,
-    /* Z near */ 1.0, /* Z far */ 10.0);
+  glLoadIdentity();
+  gluOrtho2D(0, 1, 0, 1);  
   glMatrixMode(GL_MODELVIEW);
-  gluLookAt(0.0, 0.0, 5.0,  /* eye is at (0,0,5) */
-    0.0, 0.0, 0.0,      /* center is at (0,0,0) */
-    0.0, 1.0, 0.);      /* up is in positive Y direction */
+  glLoadIdentity();
 
-  /* Adjust cube position to be asthetic angle. */
-  glTranslatef(0.0, 0.0, -1.0);
-  glRotatef(60, 1.0, 0.0, 0.0);
-  glRotatef(-20, 0.0, 0.0, 1.0);
+  glColor4f(1,0,0,1);
+  glBegin(GL_QUADS);
+  glVertex3f(0.2,0.2,0);
+  glVertex3f(0.8,0.2,0);
+  glVertex3f(0.8,0.8,0);
+  glVertex3f(0.2,0.8,0);
+  glEnd();
 
-  drawBox();
-  
-  glutPostRedisplay();
   glutSwapBuffers();
+  glutPostRedisplay();
 }
 
 void
